40bb9431ec07b27699fd33bb039b0a1e36ca529e
Gabriel Modin Bärzén <gabriel.barzen@mailfence.com>
2023-01-16 09:17:54 +0100

    added forgotten files

---
+++ 3.3/app/src/main/java/se/gabnet/vidiapp/MainActivity.java
@@ -0,0 +1,20 @@
+package se.gabnet.vidiapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState==null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.video_fragment, video.class,null)
+                    .commit();
+        }
+    }
+}

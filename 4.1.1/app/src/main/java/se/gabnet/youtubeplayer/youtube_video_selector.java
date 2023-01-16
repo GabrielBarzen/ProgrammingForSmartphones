5fde4dd0bb3a6bace58fa2d7b3f1ba2ab94eed0d
Gabriel Modin Bärzén <gabriel.barzen@mailfence.com>
2023-01-16 09:41:53 +0100

    4.1.1 done

--- 4.1.1/app/src/main/java/se/gabnet/youtubeplayer/youtube_video_selector.java
+++ 4.1.1/app/src/main/java/se/gabnet/youtubeplayer/youtube_video_selector.java
@@ -1,58 +1,71 @@
 package se.gabnet.youtubeplayer;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link youtube_video_selector#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class youtube_video_selector extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    Button video1, video2, video3, video4, video5;
+    Context context;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public youtube_video_selector() {
-        // Required empty public constructor
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment youtube_video_selector.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static youtube_video_selector newInstance(String param1, String param2) {
-        youtube_video_selector fragment = new youtube_video_selector();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        video1 = view.findViewById(R.id.video1);
+        video2 = view.findViewById(R.id.video2);
+        video3 = view.findViewById(R.id.video3);
+        video4 = view.findViewById(R.id.video4);
+        video5 = view.findViewById(R.id.video5);
+        List<Button> buttons = new ArrayList<>();
+        buttons.add(video1);
+        buttons.add(video2);
+        buttons.add(video3);
+        buttons.add(video4);
+        buttons.add(video5);
+        String[] links = {"Cv9NSR-2DwM","zqNTltOGh5c","uKYrNgbui1w","dgx9e-5_fE8","SIf559UarPE&list=OLAK5uy_norak3Q_aYAj1v8IAyUqioZjZEpdDnXx0"};
+        ArrayList<String> linksList = new ArrayList<>(Arrays.asList(links));
+
+        for (Button button : buttons) {
+            button.setOnClickListener(v -> {
+                String id = linksList.remove(0);
+                Intent youtubeAppIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + id ));
+                Intent youtubeWebIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(("https://www.youtube.com/watch?v=" + id)));
+                try {
+                    //context.startActivity(youtubeAppIntent);
+                    context.startActivity(youtubeWebIntent);
+
+                }catch (Exception e) {
+                }
+            });
+        }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
